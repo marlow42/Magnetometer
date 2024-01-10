8834d8e91fccb8d3ba9135ff50a368219a22d4ca
marlow42 <107958086+marlow42@users.noreply.github.com>
2024-01-10 15:05:38 -0500

    Add files via upload

--- magnetometer_commandline.py
+++ magnetometer_commandline.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # SERIAL
 ser1 = serial.Serial('COM7',115200)
-ser1.timeout = 0
+ser1.timeout = 2000
 # time.sleep(1)
 
 # GUI window & stringvars
@@ -19,6 +19,9 @@ z = 0
 range_x = 0
 range_y = 0
 range_z = 0
+stepx = 1
+stepy = 1
+stepz = 1
 
 # time.sleep mimic
 def wait_(s):
@@ -93,31 +96,30 @@ def moveto():
 ldata = [[],[],[]]
 lpos = [[],[],[]]
 def getdata():
-    global updates,range_x,range_y,range_z
+    global updates,range_x,range_y,range_z, stepx, stepy, stepz
     updates = "collecting data"
     global ldata, lpos
-    time1 = time.time()
-    rx = range_x
-    ry = range_y
-    rz = range_z
-    if not (rx and ry and rz):
-        return()
     ldata = [[],[],[]]
     lpos = [[],[],[]]
-    # need to add adjustable step increment size per measurement
-    # (right now it's static at 1mm)
-    rx = int(rx)
-    ry = int(ry)
-    rz = int(rz)
 
-    xc = 0
-    yc = 0
-    zc = 0
+    # define measurement ranges
+    rxi = int(range_x)
+    ryi = int(range_y)
+    rzi = int(range_z)
+    if not (rxi and ryi and rzi):
+        return()
+    
+    # add step size for measurement
+    rx = (rxi+stepx)-(rxi%stepx)+(stepx*int(rxi%stepx != 0))
+    ry = (ryi+stepy)-(ryi%stepy)+(stepy*int(ryi%stepy != 0))
+    rz = (rzi+stepz)-(rzi%stepz)+(stepx*int(rzi%stepz != 0))
+
     # c = [2,xc,yc,zc]
 
+    # move steppers to bottom left
     zero()
     print("zeroed")
-    c = [2,-rx/2,0,-rz/2] # move steppers to bottom left
+    c = [2,-rxi/2,0,-rzi/2]
     c = [str(i) for i in c]
     print("moving to bottom left:",c)
     ser1.write(bytes(",".join(c), 'utf-8'))
@@ -127,25 +129,31 @@ def getdata():
     # wait_(1)
     c = [2,0,0,0]
 
+    # collect data
     reverse = False
-
+    revx = False
+    if (rz//stepz) % 2 == 1:
+        revx = True
+    xc = 0
+    yc = 0
+    zc = 0
     print("starting algorithm")
-    for i in range(int(ry)):
+    for i in range(0,ry,stepy):
         yc = i
-        for j in range(int(rz)):
-            if i%2 == 0:
+        for j in range(0,rz,stepz):
+            if (i/stepy)%2 == 0:
                 zc = j
                 reverse = False
-            elif i%2 == 1:
-                zc = rz-j-1
+            elif (i/stepy)%2 == 1:
+                zc = rz-j-stepz
                 reverse = True
-            for k in range(int(rx)):
-                if j%2==0:
+            for k in range(0,rx,stepx):
+                if (j/stepz)%2==0:
                     xc = k
-                elif j%2 == 1:
-                    xc = rx-k-1
-                if(reverse):
-                    xc = rx-xc-1
+                elif (j/stepz)%2 == 1:
+                    xc = rx-k-stepx
+                if reverse and revx:
+                    xc = rx-xc-stepx
 
                 c = [2,xc,-yc,zc]
                 c = [str(i) for i in c]
@@ -165,7 +173,7 @@ def getdata():
                 ldata[2].append(int(r[2]))
                 # lpos.append(c[1:])
                 lpos[0].append(int(c[1]))
-                lpos[1].append(int(c[2]))
+                lpos[1].append(-int(c[2]))
                 lpos[2].append(int(c[3]))
 
                 # wait_(1.5)
@@ -176,20 +184,19 @@ def getdata():
     print(ldata)
     print(lpos)
 
-
     colors = []
     for n in range(len(ldata[0])):
         a = ldata[0][n]
         b = ldata[1][n]
         c = ldata[2][n]
         strength = np.sqrt(a**2+b**2+c**2)
+        # colors.append((5*strength,0,100))
+        colors.append((255,0,0))
 
 
     ax = plt.figure().add_subplot(projection='3d')
-    ax.quiver(lpos[0], lpos[1], lpos[2], ldata[0], ldata[1], ldata[2], length=0.1, normalize=True, pivot='mid')
+    ax.quiver(lpos[0], lpos[1], lpos[2], ldata[0], ldata[1], ldata[2], colors=colors, normalize=True, pivot='middle')
     plt.show()
-
-
 
 def test_time():
     # zero()
@@ -218,12 +225,15 @@ while True:
         moveto()
         print(time.time()-a)
     elif s == "3":
-        range_x = 2
-        range_y = 2
-        range_z = 2
+        range_x = 30
+        range_y = 10
+        range_z = 20
+        stepx = 3
+        stepy = 3
+        stepz = 3
         getdata()
-        print(ldata)
-        print(lpos)
+        # print(ldata)
+        # print(lpos)
     elif s != "":
         print(s)
         a = time.time()
